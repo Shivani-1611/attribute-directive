e45fdfd8266f06b3ef18cedae3f2b8870932a90c
shivani-16 <vibhuanant@gmail.com>
2022-05-18 23:02:48 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isStyleApplied= false;
   title = 'attribute-directives';
+  highlightColor='white';
+  highlight(color:string){
+    this.highlightColor= color;
+    
+  }
+  changeStyle(){
+    this.isStyleApplied= !this.isStyleApplied;
+  }
 }
